f0bfc0387f940158f3c7666484d24a7c4c985930
Umidjon1717 <umidizzatullayev1717@gmail.com>
2024-12-03 21:58:31 +0500

    service

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,55 @@ import Home from "./pages/home/Home";
 import Layout from "./pages/layout/Layout";
 import LogIn from "./pages/login/LogIn";
 import NotFound from "./pages/NotFound/NotFound.jsx";
+import img_1 from "./assets/image-22.png";
+import img_2 from "./assets/Image-23.png";
+import img_3 from "./assets/Image-24.png";
+import img_4 from "./assets/Image-25.png";
+import img_5 from "./assets/Image-26.png";
+
+
+const cardData = [
+  {
+    image: img_1,
+    title: "Technical Support",
+    about:"Best in class tech support for your company. We become your tech backbone ",
+    description:
+      "Through True Rich Attended does no end it his mother since real had half every him case in packages enquire we up ecstatic unsatiable saw his giving Remain expense you position concluded. Through True Rich Attended does no end it his mother since real had half every.",
+      bg:'white'
+  },
+  {
+    image: img_2,
+    title: "Developoment",
+    about:"Bring your ideas to reality with certified team of developers, working with latest technologies ",
+    description:
+      "Through True Rich Attended does no end it his mother since real had half every him case in packages enquire we up ecstatic unsatiable saw his giving Remain expense you position concluded. Through True Rich Attended does no end it his mother since real had half every.",
+      bg:'#ECF8F9'
+  },
+  {
+    image: img_3,
+    title: "AWS/Azure ",
+    about:"We help you deploy, manage and secure your application on leading web services",
+    description:
+      "Through True Rich Attended does no end it his mother since real had half every him case in packages enquire we up ecstatic unsatiable saw his giving Remain expense you position concluded. Through True Rich Attended does no end it his mother since real had half every.",
+      bg:'white'
+  },
+  {
+    image: img_4,
+    title: "Consulting",
+    about:"Get advice from world class professionals ",
+    description:
+      "Through True Rich Attended does no end it his mother since real had half every him case in packages enquire we up ecstatic unsatiable saw his giving Remain expense you position concluded. Through True Rich Attended does no end it his mother since real had half every.",
+      bg:'#F9F9FF'
+  },
+  {
+    image: img_5,
+    title: "Information Technology",
+    about:"We want to get local identification in every corner of the world in this era of global citizenship.",
+    description:
+      "Through True Rich Attended does no end it his mother since real had half every him case in packages enquire we up ecstatic unsatiable saw his giving Remain expense you position concluded. Through True Rich Attended does no end it his mother since real had half every.",
+      bg:'white'
+  },
+];
 
 function App() {
   return (
@@ -15,7 +64,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="/" element={<Home />} />
-          <Route path="/service" element={<Service />} />
+          <Route path="/service" element={<Service cardData={cardData} />} />
           <Route path="/company" element={<Company />} />
           <Route path="/career" element={<Career />} />
           <Route path="*" element={<NotFound />} />
